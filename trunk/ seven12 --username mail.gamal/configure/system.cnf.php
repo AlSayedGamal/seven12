4d42fe60e73edc01af14249d50a4eb389d958eb4
mail.gamal <mail.gamal@6c95c079-de52-0410-9700-33c6c169d3dd>
2008-07-25 20:10:20 +0000

    duplicated

--- trunk/ seven12 --username mail.gamal/configure/system.cnf.php
+++
@@ -1,6 +0,0 @@
-<?php
-define( 'INSTALL_PATH',str_replace('index.php','',$_SERVER['SCRIPT_FILENAME']));
-define( 'SITE_NAME',"UM El-Qura" );
-define( 'AUTHUR',"El-Sayed G. AbdulAzem" );
-define( 'VERSION',"0.1");
-?>
--- trunk/ seven12 --username mail.gamal/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-?>
-<?php
-$iWasThere=true;
-include ('shared/core.lib.php');
-$out=start('dev');
-echo $out;
-?>
