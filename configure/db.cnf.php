533c55e7513217629145264c82ba6aef2ccb87b4
clever.netman <clever.netman@6c95c079-de52-0410-9700-33c6c169d3dd>
2009-06-12 16:42:28 +0000

    New Version

--- configure/db.cnf.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB',0);
-define('DB_HOST','localhost');
-define('DB_NAME','shouts');
-define('DB_USER','root');
-define('DB_PASS','');
-?>
--- configure/system.cnf.php
+++
@@ -1,8 +0,0 @@
-<?php
-define( 'INSTALL_PATH',str_replace('index.php','',$_SERVER['SCRIPT_FILENAME']));
-define( 'RUN_PATH',"http://".$_SERVER['SERVER_NAME'].str_replace('index.php','index',$_SERVER['SCRIPT_NAME']));
-define( 'LINK',str_replace('/index','',RUN_PATH));
-define( 'SITE_NAME',"7" );
-define( 'AUTHUR',"El-Sayed G. AbdulAzem" );
-define( 'VERSION',"0.1");
-?>
