44ba1d9d1f08cff7f6ce7c9050a2f75bdb1e590f
clever.netman <clever.netman@6c95c079-de52-0410-9700-33c6c169d3dd>
2009-06-12 22:53:14 +0000

    Fixed bugs in validate() and render_var()

--- trunk/shared/validate.lib.php
+++ trunk/shared/validate.lib.php
@@ -1,31 +1,46 @@
 <?php
-function validate(){
-//empty all the http request variables
+
+function validate() {
+	//empty all the http request variables
+	//---
+	$req = array();
+
+	foreach($_GET as $k=>$v) {
+		$req[$k] = $v;
+		unset($_REQUEST[$k]);
+	}
+	  
+	foreach($_POST as $k=>$v){
+		$req[$k] = $v;
+		unset($_REQUEST[$k]);
+	}
+	  
+	unset($_GET);
+	unset($_POST); 
 //---
-$req= array();
-	  foreach($_GET as $k=>$v){
-			$req[$k]=$v;
-			unset($_REQUEST[$k]);
-	  }
-	  foreach($_POST as $k=>$v){
-			$req[$k]=$v;
-			unset($_REQUEST[$k]);
-	  }
-	  unset($_GET);
-	  unset($_POST);
-//---
-	if ( isset( $req ) ){
-			foreach($req as $k=>$v){
-				if(is_string($v)){
-						$allowedTags='<a><br><b><h1><h2><h3><h4><i>' .
-								'<img><li><ol><p><strong><table>' .
-								'<tr><td><th><u><ul>';
-						strip_tags($v,$allowedTags);
-						htmlentities($v);
-						addslashes($v);
-					}
-				}
+	if ( isset( $req ) ) {
+		foreach($req as $k => &$v) {
+			if(is_string($v)) {
+				$allowedTags='<a><br><b><h1><h2><h3><h4><i>' .
+							'<img><li><ol><p><strong><table>' .
+							'<tr><td><th><u><ul>';
+				$v = strip_tags($v,$allowedTags);
+				$v = htmlentities($v);
+			}
 		}
-	return $req;
+	}
+
+	// add slashes only if magic_quotes_gpc is OFF
+	// to prevent double escaping
+	if ( get_magic_quotes_gpc() == 0 ) {
+		array_walk($req, '_addslashes');
+	}
+	
+	return strtolower($req);
 }
-?>
+
+function _addslashes( &$val ) {
+	$val = addslashes($val);
+}
+
+// EOF validate.lib.php
