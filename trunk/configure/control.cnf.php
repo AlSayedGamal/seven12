215937e0c2bb3aaa275b24710a690d990fc31698
clever.netman@gmail.com <clever.netman@gmail.com@6c95c079-de52-0410-9700-33c6c169d3dd>
2009-11-24 20:44:47 +0000

    Removed 'index' word from the URI

--- trunk/configure/control.cnf.php
+++ trunk/configure/control.cnf.php
@@ -3,9 +3,9 @@ define('SCAFF',"");
 //NOTE:this array values cann't be the string 'scaff' OR 'Demo' these controller names are reserved for the framework basic installation.
 //you can avoid this by removing these controllers if you don't need them. [after deployment]
 $arInstldContrl = array(
-		          'default'	  =>'home',
-		         's'		  =>'scaff',
-			     'acp'	  =>'acp',
+		          'default'	  => 'home',
+		          's'		  => 'scaff',
+			      'acp'		  => 'acp',
 // add your controllers
 				
 			);
--- trunk/shared/loader.lib.php
+++ trunk/shared/loader.lib.php
@@ -15,7 +15,7 @@ class Loader {
 	function __construct() {
 		global $iWasThere;
 
-		$this->libPath 	= INSTALL_PATH . "shared";
+		$this->libPath 		= INSTALL_PATH . "shared";
 		$this->controlPath 	= INSTALL_PATH . "control";
 		$this->modelPath 	= INSTALL_PATH . "model";
 		$this->viewPath  	= INSTALL_PATH . "view";
@@ -30,67 +30,87 @@ class Loader {
 
 
 	private function _load($path, $name, $postfix) {
+	
 		global $iWasThere;
-		require($path . '/' . $name . $postfix);
+		
+		require_once($path . '/' . $name . $postfix);
+		
 		$this->loadedFiles[] = $name;
+		
 		if ($this->debug) echo $name, "<br />";
+		
 	}
 
 	/*
-		$type: noraml or class
+		$type: normal or class
 	*/
 	function lib($lib, $type = 'normal') {
+		if (is_array($lib)) {
+		
+			foreach ($lib as $l) {
+				$this->lib($l);
+			}
+			return TRUE;
+			
+		}
+		
 		if (!in_array($lib, $this->loadedFiles)) {
 
 			$this->_load($this->libPath, $lib, '.lib.php');
 
-			if ($type == "class") {
+		/*	if ($type == "class") {
 				if (class_exists($model, false)) {
 					$$lib = new $lib;
 				} else {
 					trigger_error("Unable to load class: $lib", E_USER_WARNING);
 				}
 			}
+		*/
 			return TRUE;
 		}
-		return;
+
 	}
 
 	function model($model, $loader = "") {
+		if (is_array($model)) {
+			foreach ($model as $m) {
+				$this->model($m);
+			}
+			return true;
+		}
+		
 		if (!in_array($model, $this->loadedFiles)) {
 			$this->_load($this->modelPath, $model, '.php');
 
 			if ($this->debug) echo "$model => $loader <br />";
 
-			if (class_exists($model, false)) {
+#			if (class_exists($model, false)) {
 				//$$model = new $model;
-			} else {
-				trigger_error("Unable to load class: $model", E_USER_WARNING);
-			}
+#			} else {
+#				exit("Unable to load class: $model");
+#			}
 
 		}
 	}
 
 	function control($control) {
 		if (!in_array($control, $this->loadedFiles)) {
+		
 			if ($control == "control") {
 				$this->_load($this->controlPath, $control, '.php');
 			} else {
 				$this->_load($this->controlPath, $control, '.c.php');
 			}
 
-			if (class_exists($control, false)) {
+#			if (class_exists($control, false)) {
 				//$$control = new $control;
-			} else {
-				trigger_error("Unable to load class: $control", E_USER_WARNING);
-			}
+#			} else {
+#				exit("Unable to load class: $control");
+#			}
 
 		}
 	}
-	
-	function from($path) { // ()->file()
-		
-	}
 
 }
+
 ?>
